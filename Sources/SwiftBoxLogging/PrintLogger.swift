68cd36b33582b6ce17bb7f2494376e81a3cee28b
Aleksander Lorenc <lorencaleksander@gmail.com>
2021-03-14 19:17:55 +0100

    Removes unused property.

--- Sources/SwiftBoxLogging/PrintLogger.swift
+++ Sources/SwiftBoxLogging/PrintLogger.swift
@@ -3,12 +3,13 @@ import Foundation
 
 public class PrintLogger: LoggerProtocol {
     public var level: Logger.Level
-    public var showLocation: Bool
     public let dateFormatter: DateFormatter
 
-    public init(level: Logger.Level = .info, showLocation: Bool = false, dateFormat: String = "yyyy-MM-dd HH:mm:ss ZZZZZ") {
+    public init(
+        level: Logger.Level = .info,
+        dateFormat: String = "yyyy-MM-dd HH:mm:ss ZZZZZ"
+    ) {
         self.level = level
-        self.showLocation = showLocation
         self.dateFormatter = DateFormatter()
         self.dateFormatter.dateFormat = dateFormat
     }
